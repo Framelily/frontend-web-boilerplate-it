0774599d64dfcb9840be23a2619da7d9a9dfef53
it <ittphon1995@gmail.com>
2026-04-01 18:02:46 +0700

    feat: wire MSW to Next.js via instrumentation hook

---
+++ src/instrumentation.ts
@@ -0,0 +1,6 @@
+export async function register() {
+  if (process.env.NEXT_PUBLIC_MOCK_API === 'true' && process.env.NODE_ENV !== 'production') {
+    const { initMocks } = await import('./mocks')
+    await initMocks()
+  }
+}
